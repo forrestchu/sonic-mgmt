df6be24a34811b5f99bcd215e5eb9f7956027f0f
yaopei.yp <yaopei.yp@alibaba-inc.com>
2022-05-16 14:10:46 +0800

    remove debug info

--- spytest/tests/routing/MC_SCALE/test_mc_scale.py
+++ spytest/tests/routing/MC_SCALE/test_mc_scale.py
@@ -49,106 +49,6 @@ from mc_vars import data
 
 #data = SpyTestDict()
 
-def var_init():
-    global vars
-    vars = st.ensure_min_topology("D1D2:4","D1T1:6","D2T1:2")
-    data.ecmp_503_504_dut1_dut2_portlist = ['Ethernet12', 'Ethernet16', 'Ethernet28', 'Ethernet32', 'Ethernet44', 'Ethernet60']
-    data.ecmp_503_504_dut_tg_portlist = ['Ethernet109', 'Ethernet110']
-    data.ecmp_501_502_dut1_dut2_portlist = ['Ethernet92', 'Ethernet96', 'Ethernet108']
-    data.ecmp_501_502_dut_RJ_portlist = ['Ethernet8', 'Ethernet9', 'Ethernet10', 'Ethernet11']
-    data.ecmp_501_502_dut_tg_portlist = ['Ethernet111', 'Ethernet112']
-    data.dut_ecmp_scale_start_subintf = '201'
-    data.dut_ecmp_scale_subintf_num = 13
-    data.dut_bfd_port_list = ['Ethernet12']
-    data.dut1_vrf1_ip_addr = ["12.109.104.2", "12.110.104.2", "12.111.100.2", " 12.112.100.2"]
-    data.dut1_vrf1_ipv6_addr = ["fd40:12:109:104::2", "fd40:12:110:104::2", "fd40:12:111:100::2", "fd40:12:112:100::2"]
-    data.dut1_vrf1_id = ["503", "503", "501", "501"]
-    data.dut1_vrf2_ip_addr = ["12.109.106.2", "12.110.106.2", "12.111.102.2", "12.112.102.2"]
-    data.dut1_vrf2_ipv6_addr = ["fd40:12:109:106::2", "fd40:12:110:106::2", "fd40:12:111:102::2", "fd40:12:112:102::2"]
-    data.dut1_vrf2_id = ["504", "504", "502", "502"]
-    data.dut_traffic_vrf_name = {"501": "long-vrf-501", "502": "long-vrf-502", 
-                                "503": "long-vrf-503", "504": "long-vrf-504"}
-    data.dut_tg_bfd_vrf_name = {"TG1_1":"long-vrf-TG-bfd1", "TG1_2":"long-vrf-TG-bfd2",
-                                "TG1_3":"long-vrf-TG-bfd3", "TG1_4":"long-vrf-TG-bfd4"}
-    data.dut1_dut2_bfd_vrf_name = ["long-vrf-dut-bfd1"]
-
-    data.dut2_vrf1_ip_addr = ["11.109.104.1", "11.110.104.1"]
-    data.dut2_vrf1_ipv6_addr = ["fd40:11:109:104::1", "fd40:11:110:104::1"]
-    data.dut2_vrf1_id = ["503", "503"]
-    data.dut2_vrf2_ip_addr = ["11.109.106.1", "11.110.106.1"]
-    data.dut2_vrf2_ipv6_addr = ["fd40:11:109:106::1", "fd40:11:110:106::1"]
-    data.dut2_vrf2_id = ["504", "504"]
-
-    data.dut3_vrf1_ip_addr = ["11.61.100.1", "11.62.100.1"]
-    data.dut3_vrf1_id = ["501", "501"]
-    data.dut3_vrf2_ip_addr = ["11.61.102.1", "11.62.102.1"]
-    data.dut3_vrf2_id = ["502", "502"]
-
-    data.dut1_bfdv4_start_ip_addr = ["109.1.0.1","110.2.0.1","111.3.0.1", "112.4.0.1"]
-    data.dut1_bfdv6_start_ip_addr = ["109:1::1","110:2::1","111:3::1", "112:4::1"]
-    data.dut2_bfdv4_start_ip_addr = ["109.1.0.1","110.2.0.1"]
-    data.dut2_bfdv6_start_ip_addr = ["109:1::1","110:2::1"]
-    data.tg1_bfdv4_start_ip_addr = ["109.1.0.2","110.2.0.2","111.3.0.2", "112.4.0.2"]
-    data.tg1_bfdv6_start_ip_addr = ["109:1::2","110:2::2","111:3::2", "112:4::2"]
-    data.tg2_bfdv4_start_ip_addr = ["109.1.0.2","110.2.0.2"]
-    data.tg2_bfdv6_start_ip_addr = ["109:1::2","110:2::2"]
-
-    data.tg1_vrf1_ip_addr = ["12.109.104.1", "12.110.104.1", "12.111.100.1", "12.112.100.1"]
-    data.tg1_vrf1_ipv6_addr = ["fd40:12:109:104::1", "fd40:12:110:104::1", "fd40:12:111:100::1", "fd40:12:112:100::1"]
-    data.tg1_vrf2_ip_addr = ["12.109.106.1", "12.110.106.1", "12.111.102.1", "12.112.102.1"]
-    data.tg1_vrf2_ipv6_addr = ["fd40:12:109:106::1", "fd40:12:110:106::1", "fd40:12:111:102::1", "fd40:12:112:102::1"]
-    data.tg1_vrf1_router_prefix_list = ["202.1.0.0", "202.1.0.0", "204.1.0.0", "204.1.0.0"]
-    data.tg1_vrf1_router_v6_prefix_list = ["3000:1::1", "3000:1::1", "3000:2::1", "3000:2::1"]
-    data.tg1_vrf2_router_prefix_list = ["202.2.0.0", "202.2.0.0", "204.2.0.0", "204.2.0.0"]
-    data.tg1_vrf2_router_v6_prefix_list = ["3001:1::1", "3001:1::1", "3001:1::1", "3001:1::1"]
-    data.tg1_vrf1_router_count_list = ["100000","100000","50000","50000"]
-    data.tg1_vrf1_router_v6_count_list = ['50000', '50000','50000','50000']
-    data.tg1_vrf2_router_count_list = ["100000","100000","50000", "50000"]
-    data.tg1_vrf2_router_v6_count_list = ['50000', '50000','50000','50000']
-    data.tg1_router_prefix_length = "31"
-    data.tg1_router_v6_prefix_length = "64"
-
-    data.tg2_vrf1_ip_addr = ["11.109.104.2", "11.110.104.2"]
-    data.tg2_vrf1_ipv6_addr = ["fd40:11:109:104::2", "fd40:11:110:104::2"]
-    data.tg2_vrf2_ip_addr = ["11.109.106.2", "11.110.106.2"]
-    data.tg2_vrf2_ipv6_addr = ["fd40:11:109:106::2", "fd40:11:110:106::2"]
-    data.tg2_vrf1_router_prefix = "200.1.0.0"
-    data.tg2_vrf1_router_v6_prefix = "4000:1::1"
-    data.tg2_vrf2_router_prefix = "200.2.0.0"
-    data.tg2_vrf2_router_v6_prefix = "4001:1::1"
-    data.tg2_router_prefix_length = "32"
-    data.tg2_router_v6_prefix_length = "64"
-    data.tg2_router_count = "10000"
-
-    data.tg3_vrf1_ip_addr = ["11.61.100.2", "11.62.100.2"]
-    data.tg3_vrf2_ip_addr = ["11.61.102.2", "11.62.102.2"]
-    data.tg3_vrf1_router_prefix = "206.1.0.0"
-    data.tg3_vrf2_router_prefix = "206.2.0.0"
-    data.tg3_router_prefix_length = "32"
-    data.tg3_router_count = "10000"
-
-    data.tg1_vrf_bgp_as = "4200015158"
-    data.tg2_vrf_bgp_as = "4200015156"
-    data.tg3_vrf_bgp_as = "4200015159"
-    data.dut1_vrf_bgp_as = "4200015155"
-    data.dut2_vrf_bgp_as = "4200015154"
-    data.dut3_vrf_bgp_as = "4200015157"
-
-    data.dut2_v4_start_ip_addr = ["109.1.0.1","110.2.0.1"]
-    data.dut2_v6_start_ip_addr = ["109:1::1","110:2::1"]
-    data.tg2_v4_start_ip_addr = ["109.1.0.2","110.2.0.2"]
-    data.tg2_v6_start_ip_addr = ["109:1::2","110:2::2"]
-
-    data.dut_bfd_timer = "100"
-    data.tg_bfd_timer = "100"
-    data.traffic_rate_precent = "99.9"
-    data.tg_list = []
-    data.tg_ph_list = []
-    data.tg1_handle = [0,0,0,0]
-    data.tg2_handle = [0,0]
-    data.tg3_handle = [0,0]
-    data.streams = {}
-
 def verify_ping(src_obj,port_handle,dev_handle,dst_ip,ping_count=5,exp_count=5):
     ping_count,exp_count = int(ping_count),int(exp_count)
     if src_obj.tg_type == 'stc':
@@ -275,7 +175,6 @@ def ixia_bfd_params_modify(bfd_handler, ipver, flap_interval='0', tx_interval='1
 def mc_scale_module_hooks(request):
     #add things at the start of this module
 
-    #var_init()
     global vars
     vars = st.ensure_min_topology("D1D2:4","D1T1:6","D2T1:2")
     (data.tg_list, data.tg_ph_list) = get_handles()
@@ -297,7 +196,6 @@ def mc_scale_module_hooks(request):
     vrfs_traffic_v6_add()
     get_dut_ip()
     yield
-    import pdb; pdb.set_trace()
     l3_base_unconfig()
 
 @pytest.fixture(scope="function", autouse=True)

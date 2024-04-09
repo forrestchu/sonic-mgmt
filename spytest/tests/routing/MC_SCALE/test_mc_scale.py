1fe643d65f5daef6cd261314856e614b6c40abe7
yuezhou.jk <yuezhou.jk@alibaba-inc.com>
2024-04-09 21:45:47 +0800

    Fix test_read_all_bfd_counter fail

--- spytest/tests/routing/MC_SCALE/test_mc_scale.py
+++ spytest/tests/routing/MC_SCALE/test_mc_scale.py
@@ -1358,14 +1358,14 @@ def test_read_all_bfd_counter():
                 rj_interval = 300
             else:
                 rj_interval = interval
-            if tx_pps > 2*(1000/rj_interval) or tx_pps < (1000/rj_interval)/2:
+            if tx_pps > 4*(1000/rj_interval) or tx_pps < (1000/rj_interval)/2:
                 st.log("current bfd session {} tx pps:{}".format(output1[i]['peeraddress'], tx_pps))
                 err_tx_session += 1
             elif down_env1 != down_env2:
                 st.log("current bfd session {} down notfiy occur".format(output1[i]['peeraddress']))
                 err_status_session += 1                
         else:
-            if tx_pps > 2*(1000/interval) or tx_pps < (1000/interval)/2:
+            if tx_pps > 4*(1000/interval) or tx_pps < (1000/interval)/2:
                 st.log("current bfd session {} tx pps:{}".format(output1[i]['peeraddress'], tx_pps))
                 err_tx_session += 1
             elif down_env1 != down_env2:

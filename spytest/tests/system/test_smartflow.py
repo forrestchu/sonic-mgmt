bb782aae32ce67ec1fd6412682cfde5bb7ed33b2
yushuanghong <yushuanghong.ysh@alibaba-inc.com>
2024-05-17 17:54:04 +0800

    update smartflow case

--- spytest/tests/system/test_smartflow.py
+++ spytest/tests/system/test_smartflow.py
@@ -38,9 +38,9 @@ def test_smartflow():
     p1_str = 'Ether()/Dot1Q(vlan={})/IPv6(src="{}",dst="{}")'.format(smartflow_data.vlan_id, smartflow_data.outer_ipv6_src, smartflow_data.outer_ipv6_dst)
     p2_str = 'Ether()/IPv6(src="{}",dst="{}")/IPv6ExtHdrRouting()/IP(src="{}",dst="{}")/UDP(sport={},dport={})'.format(smartflow_data.outer_ipv6_src, smartflow_data.outer_ipv6_dst, smartflow_data.inner_ipv4_src, smartflow_data.inner_ipv4_dst, smartflow_data.src_port, smartflow_data.dst_port)
     p3_str = 'Ether()/Dot1Q(vlan={})/IP(src="{}",dst="{}")/UDP(sport={},dport={})/VXLAN(vni={})/Ether()/IPv6(src="{}",dst="{}")/TCP(sport={},dport={})'.format(smartflow_data.vlan_id, smartflow_data.outer_ipv4_src, smartflow_data.outer_ipv4_dst, smartflow_data.vxlan_src_port, smartflow_data.vxlan_dst_port, smartflow_data.vni, smartflow_data.inner_ipv6_src, smartflow_data.inner_ipv6_dst, smartflow_data.src_port, smartflow_data.dst_port)
-    p4_str = 'Ether()/IP(src="{}",dst="{}")'.format(smartflow_data.outer_ipv4_src, smartflow_data.outer_ipv4_dst)
+    p4_str = 'Ether()/IP(src="{}",dst="{}")/TCP(sport={},dport={})'.format(smartflow_data.outer_ipv4_src, smartflow_data.outer_ipv4_dst, smartflow_data.src_port, smartflow_data.dst_port)
 
-    cmd = '''python -c 'from scapy.all import *; p1 = {}; p2 = {}; p3 = {}; p4 = {}; pkts = [p1, p2, p3, p4]; sendp(pkts, count=10, inter=0.1, iface="{}")' '''.format(p1_str, p2_str, p3_str, p4_str, vars.D1D2P1)
+    cmd = '''python -c 'from scapy.all import *; p1 = {}; p2 = {}; p3 = {}; p4 = {}; pkts = [p1, p2, p3, p4]; sendp(pkts, count=10, inter=0.01, iface="{}")' '''.format(p1_str, p2_str, p3_str, p4_str, vars.D1D2P1)
     st.config(vars.D1, cmd)
 
     cmd = "show smartflow flow drop counter"

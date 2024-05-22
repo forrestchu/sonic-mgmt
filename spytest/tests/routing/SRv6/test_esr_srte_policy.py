8957358f3256b6bc3594388aad695c2c7f5a8cc3
guozhongfeng.gzf <guozhongfeng.gzf@alibaba-inc.com>
2024-05-22 21:06:34 +0800

    Add new sr-te policy case

--- spytest/tests/routing/SRv6/test_esr_srte_policy.py
+++ spytest/tests/routing/SRv6/test_esr_srte_policy.py
@@ -127,7 +127,7 @@ def test_srte_policy_2k_vrf_1k_policy_01():
     }
     key = "bfd-name d"
 
-    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 5, delay= 10):
+    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 20, delay= 10):
         st.report_fail("Step3: The cpath d: bfd-name d not down")
 
     ret = retry_api(check_dut_intf_tx_traffic_counters, dut2, ["Ethernet3"], 150, retry_count= 3, delay= 5)
@@ -147,7 +147,7 @@ def test_srte_policy_2k_vrf_1k_policy_01():
     }
     key = "bfd-name d"
 
-    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 5, delay= 10):
+    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 20, delay= 10):
         st.report_fail("Step5: The cpath d: bfd-name d not up")
 
     ret = retry_api(check_dut_intf_tx_traffic_counters, dut2, ["Ethernet4"], 150, retry_count= 3, delay= 5)
@@ -202,7 +202,7 @@ def test_srte_policy_2k_vrf_1k_policy_color_only_02():
     }
     key = "bfd-name d"
 
-    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 5, delay= 10):
+    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 20, delay= 10):
         st.report_fail("Step4: The cpath d: bfd-name d not down")
 
     ret = retry_api(check_dut_intf_tx_traffic_counters, dut2, ["Ethernet3"], 150, retry_count= 3, delay= 5)
@@ -222,7 +222,7 @@ def test_srte_policy_2k_vrf_1k_policy_color_only_02():
     }
     key = "bfd-name c"
 
-    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 5, delay= 10):
+    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 20, delay= 10):
         st.report_fail("Step6: The cpath c: bfd-name c not down")
 
     ret = retry_api(check_dut_intf_tx_traffic_counters, dut2, ["Ethernet2"], 150, retry_count= 3, delay= 5)
@@ -242,7 +242,7 @@ def test_srte_policy_2k_vrf_1k_policy_color_only_02():
     }
     key = "bfd-name b"
 
-    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 5, delay= 10):
+    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 20, delay= 10):
         st.report_fail("Step8: The cpath b: bfd-name b not down")
 
     ret = retry_api(check_dut_intf_tx_traffic_counters, dut2, ["Ethernet1"], 150, retry_count= 3, delay= 5)
@@ -262,7 +262,7 @@ def test_srte_policy_2k_vrf_1k_policy_color_only_02():
     }
     key = "bfd-name b"
 
-    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 5, delay= 10):
+    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 20, delay= 10):
         st.report_fail("Step10: The cpath b: bfd-name b not up")
 
     ret = retry_api(check_dut_intf_tx_traffic_counters, dut2, ["Ethernet2"], 150, retry_count= 3, delay= 5)
@@ -282,7 +282,7 @@ def test_srte_policy_2k_vrf_1k_policy_color_only_02():
     }
     key = "bfd-name c"
 
-    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 5, delay= 10):
+    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 20, delay= 10):
         st.report_fail("Step12: The cpath c: bfd-name c not down")
 
     ret = retry_api(check_dut_intf_tx_traffic_counters, dut2, ["Ethernet3"], 150, retry_count= 3, delay= 5)
@@ -302,7 +302,7 @@ def test_srte_policy_2k_vrf_1k_policy_color_only_02():
     }
     key = "bfd-name d"
 
-    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 5, delay= 10):
+    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 20, delay= 10):
         st.report_fail("Step14: The cpath d: bfd-name d not down")
 
     ret = retry_api(check_dut_intf_tx_traffic_counters, dut2, ["Ethernet4"], 150, retry_count= 3, delay= 5)
@@ -371,7 +371,7 @@ def test_srte_policy_2k_vrf_2k_policy_03():
     }
     key = "bfd-name d"
 
-    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 5, delay= 10):
+    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 20, delay= 10):
         st.report_fail("Step5: The cpath d: bfd-name a not up")
 
     #check traffic back to Ethernet4
@@ -442,7 +442,7 @@ def test_srte_policy_2k_vrf_2k_policy_color_only_04():
     }
     key = "bfd-name d"
 
-    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 5, delay= 10):
+    if not retry_api(check_bfd_state, dut2, key, check_filed, retry_count= 20, delay= 10):
         st.report_fail("Step5: The cpath d: bfd-name a not up")
 
     #check traffic back to Ethernet4
